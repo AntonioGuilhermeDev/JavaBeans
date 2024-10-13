b9b08c68d08f734b1cfcb97ed0d05f2068974e72
KioWdl <caiowonato@gmail.com>
2024-10-13 17:38:11 -0300

    adiciona o arquivo js de seletores da página de cadastro

---
+++ Assets/js/sign-up/selectors.js
@@ -0,0 +1,7 @@
+const signUpForm = document.querySelector('#sign-up-fields-form');
+const username = document.querySelector('#sign-up-user-field');
+const email = document.querySelector('#sign-up-email-field');
+const password = document.querySelector('#sign-up-password-field');
+const confirmPassword = document.querySelector('#sign-up-confirm-password-field');
+
+export { signUpForm, username, email, password, confirmPassword };
