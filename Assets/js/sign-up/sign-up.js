11d33f93934bd9ce4a129684c2be007fc74536f6
KioWdl <caiowonato@gmail.com>
2024-10-13 17:41:32 -0300

    adiciona as validações iniciais no envio do formulário de cadastro

---
+++ Assets/js/sign-up/sign-up.js
@@ -0,0 +1,16 @@
+import signUpValidate from './validate.js';
+import {signUpForm, username, email, password, confirmPassword} from './selectors.js';
+
+signUpForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    signUpValidate.clearWarnings();
+    
+    const error = signUpValidate.validateFields(username, email, password, confirmPassword);  // Usando a função importada
+    if (error) {
+        const errorMessage = signUpValidate.createWarning(error);  // Usando a função importada
+        signUpForm.insertAdjacentElement('afterbegin', errorMessage);
+    } else {
+        alert("Tudo ok")
+    }
+});
