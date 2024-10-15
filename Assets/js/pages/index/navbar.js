40f77a9cb9dfbe0b7b22cda2f8fad5e8a4f8a633
Antonio Guilherme <antonioguilhermeslima@gmail.com>
2024-10-15 14:51:56 -0300

    7º commit - Implementando função de fechar nav ao clicar nas opções

--- Assets/js/pages/index/navbar.js
+++ Assets/js/pages/index/navbar.js
@@ -10,4 +10,16 @@ function mostraMenu () {
     }
 }
 
+document.querySelectorAll('.nav-mobile-menu .nav-link').forEach(link => { // Seleciona todos os links dentro do menu mobile com a classe 'nav-link'
+    link.addEventListener('click', () => {  // Adiciona um ouvinte de evento 'click' a cada link
+        let menuMobile = document.querySelector('.nav-mobile-menu');  // Seleciona o menu mobile
+        let menuIcon = document.querySelector('.nav-mobile-icone-menu img'); // Seleciona o ícone do menu (imagem) no cabeçalho
+        let button = document.querySelector('.nav-mobile-icone-menu button'); // Seleciona o botão que abre e fecha o menu
+
+        menuMobile.classList.remove('open'); // Remove a classe 'open' do menu para fechá-lo
+        menuIcon.src = "Assets/images/navbar/menu_white_36dp.svg"; // Troca o icone de aberto do menu para o icone normal
+        button.setAttribute('aria-expanded', 'false'); // Define o atributo 'aria-expanded' do botão como 'false', indicando que o menu está fechado
+    });
+});
+
 window.mostraMenu = mostraMenu; // Torna a função visivel no escopo global
